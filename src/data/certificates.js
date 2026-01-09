c0a0acd91733d00e7b1a1e0aba5e418ce6f48d38
Avhishikta <avhishiktamaiti@gmail.com>
2026-01-09 23:43:04 +0530

    Certificate add

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,12 +1,12 @@
 export const certificates = [
   {
     id: 'cert-1',
-    title: 'certificate title will be added soon.',
-    issuer: 'issuer will be added soon.',
-    date: '2025-12',
-    description: 'description will be added soon.',
-    image: '/certificates/placeholder.svg',
-    href: '/certificates/placeholder.svg'
+    title: 'SkillQuest - Data Science Foundation',
+    issuer: 'Simplilearn SkillUp',
+    date: '2025 June 28th',
+    description: 'Certificate of Accomplishment awarded for successfully passing the SkillQuest – Data Science Foundation assessment, recognizing demonstrated knowledge and commitment to building in-demand data science skills.',
+    image: '/certificates/DS.jpg',
+    href: '/certificates/DS.jpg'
   },
   {
     id: 'cert-2',
